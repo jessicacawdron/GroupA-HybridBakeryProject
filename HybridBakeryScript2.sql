cf506b31c8a64b8c32d9929bd9f4503edff19852
Ayla <aylafertug@hotmail.com>
2022-04-20 16:09:31 +0100

    script to add sample data to SQL tables

---
+++ HybridBakeryScript2.sql
@@ -0,0 +1,21 @@
+USE hybrid_bakery;
+
+INSERT INTO address(first_line, second_line, town, postcode) VALUES ('3 Burley Road', 'Burley', 'Leeds', 'LS8 3CT'), ('61 Town Street', 'Greendale', 'Sheffield', 'S12 4QP');
+
+INSERT INTO customer(first_name, last_name, email, phone_number, home_address, billing_address) VALUES ('John', 'Smith', 'john@email.com', '01234567890', 1, 1), ('Jane', 'Jones', 'jane@email.com', '02345678901', 2, 2);
+
+INSERT INTO order_details(customer_id, order_value, order_date, order_status, delivery_address, delivery_recipient) VALUES (1, 10.00, '2022-04-18', 'confirmed', 1, 'Mr Smith'), (2, 20.00, '2022-04-19', 'despatched', 2, 'Mrs Jones');
+
+INSERT INTO order_items(order_id, product_id, quantity) VALUES (1, 1, 2), (2, 2, 1), (2, 3, 4);
+
+SELECT *
+FROM address;
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM order_details;
+
+SELECT *
+FROM order_items;
