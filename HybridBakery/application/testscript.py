df1d836ab346a878a17ccdde2b47f592e4674fa8
jessicacawdron <98054365+jessicacawdron@users.noreply.github.com>
2022-04-20 22:12:09 +0100

    Adding test for relationships

--- HybridBakery/application/testscript.py
+++ HybridBakery/application/testscript.py
@@ -1,6 +1,10 @@
 from application import db
 
 from application.models.product import Product
+from application.models.allergen import Allergen
+from application.models.product_allergen import Product_Allergen
+from application.models.address import Address
+from application.models.customer import Customer
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -14,3 +18,9 @@ session = Session()
 for product in session.query(Product).all():
     print(product.product_name, product.price, product.product_description)
 
+address = session.query(Address).filter_by(id=1).first()
+print(address.first_line, address.postcode)
+for customer in address.home:
+    print(customer.first_name, customer.last_name, 'lives at', address.first_line, address.postcode )
+for customer in address.billing:
+    print('The billing address for', customer.first_name, customer.last_name, 'is', address.first_line, address.postcode )
