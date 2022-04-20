80ee20100ebb9b28ec2a3852a6158ddd98b4fe29
Ayla <aylafertug@hotmail.com>
2022-04-20 11:24:43 +0100

    adding further ORM models for SQL tables

---
+++ HybridBakery/application/models/address.py
@@ -0,0 +1,17 @@
+from application import db
+from dataclasses import dataclass
+
+
+@dataclass
+class Address(db.Model):
+    id: int
+    first_line: str
+    second_line: str
+    town: str
+    postcode: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_line = db.Column(db.String(100), nullable=False)
+    second_line = db.Column(db.String(100), nullable=True)
+    town = db.Column(db.String(35), nullable=False)
+    postcode = db.Column(db.String(10), nullable=False)
---
+++ HybridBakery/application/models/order_items.py
@@ -0,0 +1,15 @@
+from application import db
+from dataclasses import dataclass
+
+
+@dataclass
+class Order_items(db.Model):
+    id: int
+    order_id: int
+    product_id: int
+    quantity: int
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
