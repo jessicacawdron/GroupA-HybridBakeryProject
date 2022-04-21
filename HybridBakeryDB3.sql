d91abf30e0d8a781c9c6732800f4f3c6f82b816f
Ayla <aylafertug@hotmail.com>
2022-04-21 16:36:43 +0100

    updating column names in model files

--- HybridBakeryDB3.sql
+++ HybridBakeryDB3.sql
@@ -31,6 +31,6 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 order_id INT NOT NULL,
 product_id INT NOT NULL,
 quantity INT NOT NULL,
-product_price DECIMAL(10,2) NOT NULL,
+product_price DECIMAL(10,2),
 FOREIGN KEY(order_id) REFERENCES order_detail(id),
 FOREIGN KEY(product_id) REFERENCES product(id));
