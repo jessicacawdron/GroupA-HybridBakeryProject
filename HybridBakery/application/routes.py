f34208b6bdb8f621b2c31dbdc41641e281dd930e
Sarah Beckett <shbeckett@gmail.com>
2022-04-22 20:39:15 +0100

    updated layout.html and service and routes for orders

--- HybridBakery/application/routes.py
+++ HybridBakery/application/routes.py
@@ -19,6 +19,23 @@ def show_products():
 
 @app.route('/products/<int:id>')
 def show_product(id):
+    #filename = "{}.jpg".format(product_name)#
     return render_template('product.html', product = service.get_product(id), message='What a tasty bake.')
+    #product_name = product_name, filename = filename,# '#
 
+@app.route('/orders', methods=['GET'])
+def show_orders():
+    error = ""
+    details = service.get_all_orders()
+    if len(details) == 0:
+        error = "There are no orders to display this week :("
+    return render_template('orders.html', order_details=details, message=error)
+
+@app.route('/orders/madethisweek', methods=['GET'])
+def show_weekly_orders():
+    error = ""
+    details = service.get_this_weeks_orders()
+    if len(details) == 0:
+        error = "There are no orders to display this week :("
+    return render_template('weeks_orders.html', order_details=details, message=error)
 
--- HybridBakery/application/service.py
+++ HybridBakery/application/service.py
@@ -1,11 +1,16 @@
 from application.models.product import Product
-
+from application.models.order_details import Order_details
 
 def get_products():
     return Product.query.all()
-
 
 def get_product(id):
     product = Product.query.filter_by(id=id).first()
     return product
 
+def get_all_orders():
+    return Order_details.query.all()
+
+def get_this_weeks_orders():
+    orders = Order_details.query.all()
+    return orders
